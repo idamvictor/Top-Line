5cd125386aacb7c8891c28088bbbad6036f17f43
Burna <Okechukwudaniel12@gmail.com>
2024-08-18 02:57:20 +0100

    Post

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,9 +1,87 @@
-import React from 'react'
-
+import { Button } from "@/components/ui/button";
+import Line from "@/components/ui/shared/Line";
+import React from "react";
 
 const Page = () => {
   return (
-   <></>
+    <section className='text-white max-w-[1620px] mx-auto px-5 md:px-10'>
+      <main className='my-10 space-y-8'>
+        <h1 className='font-bold text-3xl text-center'>About Us</h1>
+        <p className=' text-center font-light text-base text-slate-300'>
+          Welcome to Topline, your premier destination for financial market
+          education and empowerment. At Topline, we believe that anyone can
+          succeed in the financial markets with the right knowledge, tools, and
+          support. Our mission is to provide comprehensive education and
+          services that help you navigate the complexities of trading,
+          investing, and capital management with confidence.
+        </p>
+        <div className='whitbg w-full h-80 bg-white '></div>
+        <div className='txt_btn flex justify-between items-start  pt-10  gap-7'>
+          <p className='text-2xl lg:text-3xl lg:max-w-[500px] font-bold max-w-[350px]'>
+            We offer a wide range of services tailored to meet your needs,
+            whether you’re a beginner or a seasoned trader.
+          </p>
+          <p className='flex flex-col gap-y-5 text-left text-base max-w-[500px] font-light text-slate-300'>
+            At Topline, we are committed to your success. Our platform is built
+            on a foundation of trust, transparency, and a passion for financial
+            education. We continually strive to innovate and enhance our
+            offerings, ensuring that you have the best resources at your
+            fingertips.
+            <Button size={"sm"} className='w-fit'>
+              Join Us Today
+            </Button>
+          </p>
+        </div>
+      </main>
+      <OurTeam />
+
+      <section className=' flex justify-between items-center gap-y-10 gap-x-5 py-5 flex-wrap'>
+        <h3 className=' text-xl font-medium max-w-[500px] gap-1 flex-col flex justify-start'>
+          Ready to Unlock Your Trading Potential?
+          <span className=' font-light text-slate-300 text-sm'>
+            Our platform is designed to cater to all your financial market needs
+            in one place. With our comprehensive services, you can grow your
+            knowledge, enhance your trading, and secure your financial future.
+          </span>
+        </h3>
+        <Button size={"lg"}>Join Us Today</Button>
+      </section>
+    </section>
   );
-}
-export default Page
+};
+const OurTeam = () => {
+  const Box = ({ color }: any) => {
+    return (
+      <div className='w-64  p-3 gap-y-2 flex-col flex justify-center items-center bg-[#aa8fc010] bg-opacity-55 shadow  rounded-md'>
+        <div
+          className='img h-[17rem] w-[15rem]'
+          style={{ backgroundColor: color }}
+        >
+          {/* imageoes here */}
+        </div>
+        <p className='text-center font-medium text-base'>Name</p>
+        <p className='text-center font-light text-sm text-slate-300'>Role</p>
+      </div>
+    );
+  };
+  return (
+    <main className='py-10'>
+      <h3 className=' text-3xl flex-col'>
+        Meet our team
+        <Line />
+      </h3>
+      <p>
+        With lots of unique blocks, you can easily build a page without coding.
+        Build your next consultancy website within few minutes.
+      </p>
+
+      <div className='teams_wrapper flex-wrap gap-5	 flex justify-between items-center my-10 '>
+        <Box color='#E6474C' />
+        <Box color='#00B2A2' />
+        <Box color='#D0D0F0' />
+        <Box color='#ED948C' />
+      </div>
+    </main>
+  );
+};
+export default Page;
--- src/app/Exchange/page.tsx
+++ src/app/Exchange/page.tsx
@@ -6,15 +6,56 @@
  */
 import React from "react";
 import type { Metadata } from "next";
+import Line from "@/components/ui/shared/Line";
+import { Button } from "@/components/ui/button";
 export const metadata: Metadata = {
   title: "TopLine Exchange",
   description: "TopLine Trading",
 };
 const ExchangePage = () => {
   return (
-    <section className='overflow-x-hidden font-poppins bg-white'>
+    <section className=' text-white'>
+      <Hero />
+      <div className='white_bg text-zinc-900 my-10 h-52 bg-white flex flex-col justify-center px-5 md:px-10 gap-y-3'>
+        <h2 className=' text-3xl flex-col'>
+          Our Exchange Process
+          <Line />
+        </h2>
+        <p>
+          With lots of unique blocks, you can easily build a page without
+          coding. Build your next consultancy website within few minutes.
+        </p>
+      </div>
     </section>
   );
 };
-
+const Hero = () => {
+  const text =
+    "Our platform offers a seamless Crypto-to-Cash Exchange Service designed to provide you with a quick and secure way to convert your cryptocurrencies into fiat money. Whether you're looking to cash out your profits or convert your digital assets into usable currency, our service ensures a smooth and transparent process. With our user-friendly interface and expert support, you'll have access to your funds in no time.";
+  return (
+    <section className='px-5 lg:px-10 relative text-white flex w-full justify-center items-center min-h-fit 2xl:max-h-[700px]  h-[90vh]'>
+      <main className=' flex w-full justify-between h-[70%]  max-md:flex-col max-h-[500px]  '>
+        <div className='flex-1 flex gap-5 justify-between  items-start  p-4'>
+          <div className='text_cont max-w-[600px] md:max-w-[400px] 2xl:max-w-[700px] space-y-7'>
+            <h1 className=' text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl  font-light'>
+            Exchange Service
+            </h1>
+            <p className=' text-slate-400 text-base md:text-xs lg:text-base 2xl:text-lg '>
+              { text }
+            </p>
+            <Button
+              className='bg-foreground text-slate-100 font-light'
+              size='lg'
+            >
+              Join Us Today
+            </Button>
+          </div>
+        </div>
+        <div className='flex-1 flex justify-start  items-center '>
+          <div className='image h-full w-full max-w-[700px] rounded-xl max-h-[700px]  bg-gray-800'></div>
+        </div>
+      </main>
+    </section>
+  );
+};
 export default ExchangePage;
