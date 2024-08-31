ecd906e65d0c556541ea63ac6bf1753605606fc8
Burna <Okechukwudaniel12@gmail.com>
2024-08-31 03:31:42 +0100

    Exchange

--- src/app/Exchange/page.tsx
+++ src/app/Exchange/page.tsx
@@ -36,35 +36,62 @@ const ExchangePage = () => {
     </section>
   );
 };
+
 const Hero = () => {
   const text =
-    "Our platform offers a seamless Crypto-to-Cash Exchange Service designed to provide you with a quick and secure way to convert your cryptocurrencies into fiat money. Whether you're looking to cash out your profits or convert your digital assets into usable currency, our service ensures a smooth and transparent process. With our user-friendly interface and expert support, you'll have access to your funds in no time.";
-  return (
-    <section className=' max-md:pt-3 px-5 lg:px-10 relative text-white flex w-full justify-center items-center min-h-fit 2xl:max-h-[700px]  md:h-[90vh]'>
-      <main className='gap-y-5 flex w-full justify-between h-[70%]  max-md:flex-col md:max-h-[500px]  '>
+       "Our platform offers a seamless Crypto-to-Cash Exchange Service designed to provide you with a quick and secure way to convert your cryptocurrencies into fiat money. Whether you're looking to cash out your profits or convert your digital assets into usable currency, our service ensures a smooth and transparent process. With our user-friendly interface and expert support, you'll have access to your funds in no time.";
+      return (
+    <section className=' exchangeHero bg-background  px-5 lg:px-10 relative text-white flex w-full justify-center items-center min-h-fit h-[80vh] 2xl:max-h-[600px]  md:h-[80vh] md:max-h-[600px]'>
+      <main className=' z-10 flex w-full justify-between md:h-[70%] gap-y-5 max-md:flex-col md:max-h-[500px]  '>
         <div className='flex-1 flex gap-5 justify-between  items-start  p-4'>
-          <div className='text_cont max-w-[600px] max-md:h-[60vh] max-md:flex flex-col justify-center md:max-w-[400px] 2xl:max-w-[700px] space-y-7'>
-            <h1 className=' text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl  font-light'>
-              Exchange Service
+          <div className='text_cont  space-y-7'>
+            <h1 className=' text-3xl md:text-4xl lg:text-5xl xl:text-6xl  font-light'>
+            Exchange Service
             </h1>
-            <p className=' text-slate-400 text-base md:text-xs lg:text-base 2xl:text-lg '>
-              {text}
+            <p className=' text-slate-300 text-base md:text-base 2xl:text-lg max-w-[600px] '>
+              { text }
             </p>
             <Button
-              className='bg-foreground text-slate-100 font-light w-fit'
+              className='bg-foreground text-slate-100 font-light text-lg'
               size='lg'
             >
-              Join Us Today
+              Join Now
             </Button>
           </div>
-        </div>
-        <div className='flex-1 flex justify-start items-center '>
-          <div className='image h-full w-full max-w-[700px] rounded-xl max-h-[700px] min-h-[500px] bg-gray-800'></div>
         </div>
       </main>
     </section>
   );
 };
+// const Hero = () => {
+//   const text =
+//     "Our platform offers a seamless Crypto-to-Cash Exchange Service designed to provide you with a quick and secure way to convert your cryptocurrencies into fiat money. Whether you're looking to cash out your profits or convert your digital assets into usable currency, our service ensures a smooth and transparent process. With our user-friendly interface and expert support, you'll have access to your funds in no time.";
+//   return (
+//     <section className=' max-md:pt-3 px-5 lg:px-10 relative text-white flex w-full justify-center items-center min-h-fit 2xl:max-h-[700px]  md:h-[90vh]'>
+//       <main className='gap-y-5 flex w-full justify-between h-[70%]  max-md:flex-col md:max-h-[500px]  '>
+//         <div className='flex-1 flex gap-5 justify-between  items-start  p-4'>
+//           <div className='text_cont max-w-[600px] max-md:h-[60vh] max-md:flex flex-col justify-center md:max-w-[400px] 2xl:max-w-[700px] space-y-7'>
+//             <h1 className=' text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl  font-light'>
+//               Exchange Service
+//             </h1>
+//             <p className=' text-slate-400 text-base md:text-xs lg:text-base 2xl:text-lg '>
+//               {text}
+//             </p>
+//             <Button
+//               className='bg-foreground text-slate-100 font-light w-fit'
+//               size='lg'
+//             >
+//               Join Us Today
+//             </Button>
+//           </div>
+//         </div>
+//         <div className='flex-1 flex justify-start items-center '>
+//           <div className='image h-full w-full max-w-[700px] rounded-xl max-h-[700px] min-h-[500px] bg-gray-800'></div>
+//         </div>
+//       </main>
+//     </section>
+//   );
+// };
 const Faq = () => {
   return (
     <section className='text-white max-w-[1620px] mx-auto px-5 md:px-10 space-y-7'>
